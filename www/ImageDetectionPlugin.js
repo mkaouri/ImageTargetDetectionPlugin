e145d95ab1413ab5a1e05410fc2b0f84d778a61d
a31859 <dnva@DevilsMac.local>
2016-04-20 16:10:33 +0100

    add pattern index to set pattern function

--- www/ImageDetectionPlugin.js
+++ www/ImageDetectionPlugin.js
@@ -5,8 +5,8 @@ var ImageDetectionPlugin = function () {
   this.startProcessing = function (bool, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "ImageDetectionPlugin", "startProcessing", [bool]);
   };
-  this.setPattern = function (pattern, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "ImageDetectionPlugin", "setPattern", [pattern]);
+  this.setPattern = function (pattern, index, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "ImageDetectionPlugin", "setPattern", [pattern, index]);
   };
   this.isDetecting = function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "ImageDetectionPlugin", "isDetecting", []);
